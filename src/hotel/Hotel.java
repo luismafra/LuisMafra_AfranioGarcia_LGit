fe11da2ab566e9fac70af606c3c1ee93ea58ee3a
Luis Mafra <luis.mafra@ccc.ufcg.edu.br>
2018-10-18 16:44:10 -0300

    Atualização do projeto

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -1,9 +1,31 @@
 package hotel;
 
 public class Hotel {
-	
+
 	Recepcao recepcao;
-	
-	
-	
+
+	public Hotel() {
+		recepcao = new Recepcao();
+	}
+
+	public void checkin(String nome, String tipo, int idade, int dias, double valor) {
+		recepcao.checkin(nome, tipo, idade, dias, valor);
+	}
+
+	public void checkout(String nome) {
+		recepcao.checkout(nome);
+	}
+
+	public int getNumDeHospedes() {
+		return recepcao.getNumDeHospedes();
+	}
+
+	public double getLucroTotal() {
+		return recepcao.getLucroTotal();
+	}
+
+	public String toString() {
+		return recepcao.toString();
+	}
+
 }
--- src/hotel/Recepcao.java
+++ src/hotel/Recepcao.java
@@ -1,32 +1,60 @@
 package hotel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recepcao {
 	
 	private List<Estadia> estadias;
+	private double lucro = 0;
 	
 	public Recepcao() {
 		
-		estadias = new HashList<>();
+		estadias = new ArrayList<>();
 		
 	}
 	
 	public void checkin(String nome,String tipo,int idade,int dias,double valor) {
 		
+		estadias.add(new Estadia(nome,tipo,idade,dias,valor));
+		
 	}
 	
 	public void checkout(String nome) {
+		
+		Estadia estadia = encontraEstadia(nome);
+		this.lucro += estadia.getValor() * estadia.getDias();
+		estadias.remove(estadia);
+		
+	}
+	
+	public Estadia encontraEstadia(String nome) {
+		
+		for(Estadia estadia:estadias)
+			if (estadia.getNome().equals(nome))
+				return estadia;
 		
 	}
 	
 	public int getNumDeHospedes() {
 		
+		return estadias.size();
+		
 	}
 	
 	public double getLucroTotal() {
 		
+		return lucro;
+		
 	}
 	
 	public String toString() {
+		
+		String aux = "";
+		for(Estadia estadia:estadias)
+			aux += estadia.getNome() + "(" + estadia.getTipo() + "): " + estadia.getDias() + " dias com o preço de R$ " + estadia.getValor() + "\n";
+		
+		return "Estadias:\n" + aux;
 		
 	}
 	
